7112b903e80a0a71db5fd6aa423988965bd71c43
naruto5406 <56410227+naruto5406@users.noreply.github.com>
2019-10-11 03:13:19 +0800

    Add files via upload

---
+++ PrimeChecker.cpp
@@ -0,0 +1,95 @@
+//
+// PrimeChecker.cpp
+// ds_hw1_warm_up
+//
+#include "PrimeChecker.h"
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+using std::vector;
+
+unsigned long PRIMECHECKER::PrimeChecker(unsigned long a, unsigned long b){
+		
+	vector<unsigned long> prime_list;
+		
+	if (a==1){
+		if (b == 1){
+			return 0;
+				
+		}
+		else{
+			a++;
+		}
+			
+	}
+		
+	if ((a % 2) == 0){
+		if (a == 2){
+			prime_list.push_back(2);
+			a++;
+		}
+		else{
+			a++;
+		}
+	}
+		
+
+	for(int count = a; count <= b; count = count + 2){
+			
+		if (prime_list.empty()){
+			
+			if (check_prime(count)){
+				prime_list.push_back(count);
+			}
+			else{
+				continue;
+			}
+		}
+		else{
+			
+			int flag1 = 1;
+			for(int j = 0; j < prime_list.size(); j++){
+				if ((count % prime_list[j]) == 0){
+					flag1 = 0;
+					break; 
+				}
+			
+			}
+			
+			if (flag1 == 0){
+				continue;
+			}
+			else{
+				if (check_prime(count)){
+					prime_list.push_back(count);
+				}
+				else{
+					continue;
+				}
+			}
+				
+		}
+	}
+
+	return prime_list.size(); 
+		
+	
+	
+
+}
+
+int check_prime(int num){
+	int flag = 1;
+	
+	for(int i = 3; i <= sqrt(num); i=i+2){
+		
+		if ((num%i) == 0){
+			flag = 0;
+			break;
+		}
+	
+	}
+	
+	return flag;
+}
---
+++ PrimeChecker.h
@@ -0,0 +1,18 @@
+// 
+// PrimeChecker.h
+// DS hw1 warm up
+//
+
+#ifndef _PrimeChecker_
+#define _PrimeChecker_
+
+int check_prime(int num);
+
+class PRIMECHECKER {
+	private:
+
+	public:
+	unsigned long PrimeChecker(unsigned long, unsigned long);
+};
+#endif
+
